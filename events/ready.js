7546bbbb013dc28848fd76999b006fb40541e101
Viljami Vastamäki <viljami.vastamaki@tutanota.com>
2021-11-27 13:33:05 +0200

    Add penthouse permission check

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,7 @@
 const helpers = require("../helpers/index");
 
+const { joinVoiceChannel } = require('@discordjs/voice');
+
 module.exports = (client) => {
   console.log(
     `Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`
@@ -8,6 +10,15 @@ module.exports = (client) => {
     activities: [{ name: "you", type: "WATCHING" }],
     status: "online",
   });
+
+  const channel = client.channels.cache.get("903388421811236885");
+ 
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+
 
   helpers.initBot();
   helpers.initGuilds(client.guilds.cache);
--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,3 +1,11 @@
+const {
+  getVoiceConnection,
+  createAudioPlayer,
+  createAudioResource,
+} = require("@discordjs/voice");
+
+const { join } = require("path");
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const channels = [
@@ -5,15 +13,16 @@ const channels = [
   "903351329743847444",
   "903351356809678848",
   "903351379416997939",
-  "903351398710788166",
-  "903351429400498207",
-  "903351457825292308",
-  "903351479690199050",
-  "903351506558914570",
-  "692416528317939793",
+  "903388421811236885",
 ];
 
-module.exports = async (oldMember, newMember, client) => {
+const penthouseId = "692416528317939793";
+
+module.exports = async (oldMember, newMember) => {
+  const hasPenthouseAccess = oldMember.member.roles.cache.find(
+    (role) => role.name === "Penthouse Access"
+  );
+
   if (
     oldMember.channel?.name === "Lobby" &&
     newMember.channel?.name === "Elevator"
@@ -25,6 +34,48 @@ module.exports = async (oldMember, newMember, client) => {
         console.error(err);
       }
       await sleep(900);
+    }
+
+    if (hasPenthouseAccess) {
+      const connection = getVoiceConnection(oldMember.guild.id);
+
+      const player = createAudioPlayer();
+
+      const accessGrantedSound = createAudioResource(
+        join(__dirname, "granted.mp3")
+      );
+
+      const subscription = connection.subscribe(player);
+
+      player.play(accessGrantedSound);
+
+      if (subscription) {
+        setTimeout(() => subscription.unsubscribe(), 5000);
+      }
+
+      await sleep(5000);
+
+      newMember.member.voice.setChannel(penthouseId);
+    } else {
+      const connection = getVoiceConnection(oldMember.guild.id);
+
+      const player = createAudioPlayer();
+
+      const accessDeniedSound = createAudioResource(
+        join(__dirname, "denied.mp3")
+      );
+
+      const subscription = connection.subscribe(player);
+
+      player.play(accessDeniedSound);
+
+      if (subscription) {
+        setTimeout(() => subscription.unsubscribe(), 5000);
+      }
+
+      await sleep(3500);
+
+      newMember.disconnect();
     }
   }
 };
